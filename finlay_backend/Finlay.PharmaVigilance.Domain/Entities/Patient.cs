eceb4fb599adef7398b0f7a65bf3db44eae6d667
JabelResendiz <jabelresendiz00@gmail.com>
2026-02-20 00:39:14 -0500

    add entities

--- finlay_backend/Finlay.PharmaVigilance.Domain/Entities/Patient.cs
+++ finlay_backend/Finlay.PharmaVigilance.Domain/Entities/Patient.cs
@@ -1,6 +1,10 @@
+namespace Finlay.PharmaVigilance.Domain.Entities;
 
+public class Patient : GenericEntity 
+{
+    public string Name {get;set;} = null!;
+    public string Email {get;set;} = null!;
+    public int PhoneNumber {get;set;}
+    public string Address {get;set;} = null!;
 
-
-public class Pacient {
-    
 }
---
+++ finlay_backend/Finlay.PharmaVigilance.Domain/Entities/Physician.cs
@@ -0,0 +1,8 @@
+
+namespace Finlay.PharmaVigilance.Domain.Entities;
+
+
+public class Physician : GenericEntity{
+   
+   // solo las FK
+}
