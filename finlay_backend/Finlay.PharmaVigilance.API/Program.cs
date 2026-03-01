528c22fe412b41756d6a4c4a902be582e415b65a
JabelResendiz <jabelresendiz00@gmail.com>
2026-03-01 14:43:33 -0500

    Configure ApiBehaviorOptions to return structured 400 validation errors

--- finlay_backend/Finlay.PharmaVigilance.API/Program.cs
+++ finlay_backend/Finlay.PharmaVigilance.API/Program.cs
@@ -5,15 +5,11 @@ using Finlay.PharmaVigilance.Api;
 using Finlay.PharmaVigilance.Application;
 using Finlay.PharmaVigilance.Infrastructure;
 using Finlay.PharmaVigilance.Api.Middleware;
+using Microsoft.AspNetCore.Mvc;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-//configuration so that API listens on all network interfaces
-builder.WebHost.ConfigureKestrel(options =>
-{
-    options.Listen(System.Net.IPAddress.Any, 5137);  // Escucha en todas las interfaces en el puerto 5217
-});
 
 var services = builder.Services;
 
@@ -21,6 +17,36 @@ var services = builder.Services;
 services.AddPresentation();
 services.AddAplication(builder.Configuration);
 services.AddInfrastructure(builder.Configuration);
+
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
+    {
+        var errors = context.ModelState
+            .Where(x => x.Value.Errors.Count > 0)
+            .Select(x => new
+            {
+                Field = x.Key,
+                Errors = x.Value.Errors.Select(e => e.ErrorMessage)
+            });
+
+        return new BadRequestObjectResult(new
+        {
+            success = false,
+            status = 400,
+            message = "Validation failed",
+            errors
+        });
+    };
+});
+
+
+//configuration so that API listens on all network interfaces
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Listen(System.Net.IPAddress.Any, 5137);  // Escucha en todas las interfaces en el puerto 5217
+});
 
 
 var app = builder.Build();
